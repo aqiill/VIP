fd9ce22dec7182f9414963ffed4b09c54e1c8010
haunanisaa <nisaahaunaa@gmail.com>
2022-10-19 16:23:25 +0700

    animation v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8'],
-    navigationTooltips: ['Beranda', 'Menu', ''],
+    navigationTooltips: ['Beranda', 'Menu', 'Geopark Ciletuh Pelabuhanratu', 'Kesepuhan Banten Kidul', 'Seren Taun', 'Peninggalan Sejarah', 'Contoh Peninggalan Sejarah', 'Gallery'],
     showActiveTooltip: true,
     controlArrows: false,
     slidesNavigation: true,
